7a98fce7454e0076c950f0716e94bb1abd11a24d
jyothi.dunaboyina <jyothi.dunaboyina@feuji.com>
2024-05-10 09:39:00 +0530

    comments

--- actions/mobile_actions.py
+++ actions/mobile_actions.py
@@ -2,25 +2,32 @@ from appium.webdriver.webdriver import WebDriver
 
 class Appium_Actions :
 
+    """This function is used for intilizing the driver"""
     def __init__(self,driver,implicit_wait_time = 10) :
         self.driver = driver;
         self.driver.implicitly_wait(implicit_wait_time);
 
+    """This function is used for returing the element"""
     def webElement(self,locator) :
         element = self.driver.find_element(* locator);
         return element;
 
+    """This function is used for clicking the web element"""
     def click_webelement(self,locator) :
         self.webElement(locator).click();
 
+    """This function is used for entering the text into the web element"""
     def input_webelement(self,locator,text) :
         self.webElement(locator).send_keys(text);
 
+    """This function is used for entering the text into the web element"""
     def get_webelement_text(self,locator) :
         return self.webElement(locator).text;
 
+    """This function is used for scrolling the web element"""
     def scroll_to_element(self,locator) :
         self.driver.execute_script("window.scrollBy(0, arguments[0].offsetTop);", locator);
     
+    """This function is used for checking the web element is displayed or not"""
     def is_webelement_displayed(self,locator) :
         return self.webElement(locator).is_displayed();
--- actions/ui_actions.py
+++ actions/ui_actions.py
@@ -3,24 +3,31 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class UI_Actions :
 
+    """Intilize the webdriver"""
     def __init__(self,driver) :
         self.driver = driver;
 
+    """This function is used for clicking the web element"""
     def webElement_click(self,locator) :
         WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(locator)).click();
 
+    """This function is used for entering the text into the web element"""
     def webElement_input(self,locator,text) :
         WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(locator)).send_keys(text);
 
+    """This function is used for entering the text into the web element"""
     def webElement_text(self,locator) :
         WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(locator)).text;
 
+    """This function is used for scrolling the web element"""
     def scroll_to_element(self,locator) :
         self.driver.execute_script("window.scrollBy(0, arguments[0].offsetTop);", locator);
         WebDriverWait(self.driver,self.get_wait_time()).until(EC.visibility_of_element_located(locator));
 
+    """This function is used for checking the web element is displayed or not"""
     def webElement_isDisplayed(self,locator) :
         WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(locator)).is_displayed();
 
+    """This function is used for clearing the input fields"""
     def webElement_input_clear(self,locator) :
         WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(locator)).clear();
